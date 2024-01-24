63300fcdc85d07b0930c4b7a2bd9878e622b587b
Delian Petrov <dpetrov@saloninteractive.com>
2024-01-23 19:27:16 -0800

    load custom fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,38 @@
 import { Stack } from 'expo-router'
+import { useCallback, useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { View, Text, SafeAreaView } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    console.log('onLayoutRootView')
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack onLayout={onLayoutRootView}></Stack>
+  )
 }
 
 export default Layout;
