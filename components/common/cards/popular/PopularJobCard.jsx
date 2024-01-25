0063682f849c25d68e9597d91dc16c161f6d4f4e
Delian Petrov <dpetrov@saloninteractive.com>
2024-01-24 19:43:21 -0800

    bug fix with the props

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -4,9 +4,7 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 import styles from './popularjobcard.style'
 import { checkImageURL } from '../../../../utils'
 
-const PopularJobCard = (item, selectedJob, handleCardPress) => {
-
-  item = item.item
+const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
 
   return (
     <TouchableOpacity
